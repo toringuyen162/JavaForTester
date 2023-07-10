c9290d870b610a7a11286328d49b3d3437e04b21
toringuyen162 <toringuyen.work@gmail.com>
2023-07-10 13:46:41 +0700

    Q4

--- Answers/src/main/java/Q4/Q4Solution.java
+++ Answers/src/main/java/Q4/Q4Solution.java
@@ -1,22 +1,30 @@
 package Q4;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Q4Solution {
     //Sử dụng for loop để đảo 1 số nguyên cho trước
     public static void main(String[] args) {
-
-        reverseFor(1248546321);
+        reverseFor(528546326);
     }
 
     public static void reverseFor(int n) {
-        boolean isMinus;
+        boolean isMinus = false;
+        int init=0;
         if (n < 0) {
             n = -n;
-            isMinus=true;
+            isMinus = true;
+        }
+        if (n >= 0) {
+             init = n % 10;
+            for (int i = n / 10; i >= 1; i /= 10) {
+                init = init * 10 + i % 10;
+            }
+        }
+        if (isMinus) {
+            System.out.println("So dao nguoc la: -" + init);
+        } else {
+            System.out.println("So dao nguoc la: " + init);
         }
 
     }
+
 }
