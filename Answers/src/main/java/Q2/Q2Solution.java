9cee7585629b8784efcf7813977a9e46271d163f
toringuyen162 <toringuyen.work@gmail.com>
2023-07-10 10:06:13 +0700

    update

--- Answers/src/main/java/Q2/Q2Solution.java
+++ Answers/src/main/java/Q2/Q2Solution.java
@@ -5,7 +5,9 @@ import java.util.Arrays;
 
 public class Q2Solution {
     public static void main(String[] args) {
-        System.out.println("HelloWorld!");
+
+        sortArray2();
+        EscSort();
     }
 
     //không sử dụng sort()
@@ -24,12 +26,45 @@ public class Q2Solution {
         System.out.println(Arrays.toString(a));
     }
 
-    public static void sortArray2(){
+    public static void sortArray2() {
         Integer[] b = {1, 3, 32, 4, 734, 3, -1, -234, 0};
         ArrayList<Integer> arrayList = new ArrayList<Integer>(Arrays.asList(b));
-        ArrayList<Integer> finalArray = new ArrayList<Integer>();
-        for (int i=0;i< arrayList.size();i++);
-            finalArray.add(arrayList.in)
+        ArrayList<Integer> outPut = new ArrayList<Integer>();
+        int minIndex;
+        while (!arrayList.isEmpty()) {
+            int min = arrayList.get(0);
+            minIndex = 0;
+            for (int i = 1; i < arrayList.size(); i++) {
+                if (min > arrayList.get(i)) {
+                    min = arrayList.get(i);
+                    minIndex = i;
+                }
+            }
+            outPut.add(min);
+            arrayList.remove(minIndex);
+        }
+        System.out.println("Sorted Array: "+ outPut);
+    }
 
+    public static void EscSort() {
+        Integer[] a = {1, 3, 32, 4, 734, 3, -1, -234, 0};
+        if (a != null) {
+            for (int i = 0; i < a.length; i++) {
+                for (int j = 0; j < a.length - 1; j++) {
+                    if (a[j] >= a[j + 1]) {
+                        int temp = 0;
+                        temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                    }
+                }
+            }
+            System.out.print("Array after EscSort: ");
+            for (int i = 0; i < a.length; i++) {
+                System.out.print(a[i] + " ");
+            }
+        } else {
+            System.out.println("Null array.");
+        }
     }
 }
