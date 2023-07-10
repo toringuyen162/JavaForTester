cfa99942ecf15359b4c261ecb6d630a9dc0b0e4d
toringuyen162 <toringuyen.work@gmail.com>
2023-07-10 15:56:09 +0700

    1

--- Answers/src/main/java/Solution/Q10Solution.java
+++ Answers/src/main/java/Solution/Q10Solution.java
@@ -1,37 +1,46 @@
 package Solution;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Q10Solution {
     public static void main(String[] args) {
-    daysInMonth("02/2024");
+        daysInMonth("05/2024");
     }
 
-    public static boolean isLeapYear(int y){
-        if(y<=0){
+    public static boolean isLeapYear(int y) {
+        if (y <= 0) {
             throw new IllegalArgumentException("Nam phai la so duong");
         }
-        if((y%4==0 && y%100!= 0)||(y%400==0)){
+        if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0)) {
             return true;
         } else {
             return false;
         }
     }
-    public static void daysInMonth(String input){
-        int m = Integer.parseInt(input.substring(0,2));
-        int y=Integer.parseInt(input.substring(3));
-        int[] day30 = {4,6,9,11};
-        if (m==2) {
+
+    public static void daysInMonth(String input) {
+        int m = Integer.parseInt(input.substring(0, 2));
+        int y = Integer.parseInt(input.substring(3));
+        int[] day30 = {4, 6, 9, 11};
+
+        if (m == 2) {
             if (isLeapYear(y) == false) {
-                System.out.println(input +" co 28 ngay");
-            } else System.out.println(input+" co 29 ngay");
+                System.out.println(input + " co 28 ngay");
+            } else System.out.println(input + " co 29 ngay");
         } else {
-            for (int i:day30){
-                if (m==i){
-                    System.out.println(input+" co 30 ngay");
+            boolean have30Days = false;
+            for (int i : day30) {
+                if (m==i) {
+                    have30Days = true;
+                    break;
                 }
-                else System.out.println(input+ "co 31 ngay");
             }
+            if (have30Days == true) {
+                System.out.println(input + " co 30 ngay");
+            } else System.out.println(input + " co 31 ngay");
         }
     }
 }
+
+
