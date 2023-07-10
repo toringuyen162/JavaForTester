355632f77da5c8dd0389c2f982b47b95cfe66a0b
toringuyen162 <toringuyen.work@gmail.com>
2023-07-10 10:32:22 +0700

    Q3

--- Answers/src/main/java/Q2/Q2Solution.java
+++ Answers/src/main/java/Q2/Q2Solution.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 
 public class Q2Solution {
     public static void main(String[] args) {
-
         sortArray2();
         EscSort();
     }
---
+++ Answers/src/main/java/Q3/Q3Solution.java
@@ -0,0 +1,9 @@
+package Q3;
+
+public class Q3Solution {
+    public static void main(String[] args) {
+
+    }
+
+    
+}
