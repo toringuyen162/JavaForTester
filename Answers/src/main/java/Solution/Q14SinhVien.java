837b99364a0abd9e0c0a3fe1abce8d00ae54e7b9
toringuyen162 <toringuyen.work@gmail.com>
2023-07-13 16:00:29 +0700

    1

--- Answers/src/main/java/Solution/Q14SinhVien.java
+++ Answers/src/main/java/Solution/Q14SinhVien.java
@@ -1,7 +1,11 @@
 package Solution;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Q14SinhVien {
-    private String id = ("JAK-" + (int) (Math.random() * 10000));
+    private String id;
     private String name;
     private boolean sex;
     private float math;
@@ -12,11 +16,13 @@ public class Q14SinhVien {
     public Q14SinhVien() {
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public void setId() {
+        id = ("JAK-" + (int) (Math.random() * 100000));
+        System.out.println("Ma so sinh vien: "+id);
     }
 
     public void setName(String name) {
+        System.out.printf("Ten sinh vien: ");
         this.name = name;
     }
 
@@ -25,18 +31,21 @@ public class Q14SinhVien {
     }
 
     public void setMath(float math) {
+        System.out.printf("Nhap diem Toan: ");
         if (math < 0 || math > 10) {
             System.out.println("Nhập điểm từ 0-10");
         } else this.math = math;
     }
 
     public void setPhysics(float physics) {
+        System.out.printf("Nhap diem Ly: ");
         if (physics < 0 || physics > 10) {
             System.out.println("Nhập điểm từ 0-10");}
         else this.physics = physics;
 
     }
     public void setChemistry ( float chemistry){
+        System.out.printf("Nhap diem Hoa: ");
         if (chemistry < 0 || chemistry > 10) {
             System.out.println("Nhập điểm từ 0-10");
         } else this.chemistry = chemistry;
@@ -44,9 +53,30 @@ public class Q14SinhVien {
 
 
     public static void main(String[] args) {
-        Q14SinhVien sv1 = new Q14SinhVien();
-        sv1.setMath();
-    }
-    }
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("Nhap vao so sinh vien: ");
+        int num = sc.nextInt();
+
+        List<Q14SinhVien> list = new ArrayList<Q14SinhVien>();
+        for (int i=0;i<num;i++){
+            list.add(new Q14SinhVien());
+        }
+
+        for (int i=0;i<num;i++){
+            var object = list.get(i);
+            object.setId();
+            sc.next();
+            object.setName(sc.next());
+            sc.next();
+            object.setMath(sc.nextFloat());
+            sc.next();
+            object.setPhysics(sc.nextFloat());
+            sc.next();
+            object.setChemistry(sc.nextFloat());
+        }
+        sc.close();
+        }
+
+}
 
 
