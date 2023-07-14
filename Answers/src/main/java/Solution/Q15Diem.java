73919b22904066f0d62d073ff3e43cc4bcd61d46
toringuyen162 <toringuyen.work@gmail.com>
2023-07-14 16:42:26 +0700

    1

--- Answers/src/main/java/Solution/Q15Diem.java
+++ Answers/src/main/java/Solution/Q15Diem.java
@@ -1,5 +1,11 @@
 package Solution;
 
+import java.lang.management.MemoryManagerMXBean;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Q15Diem {
     private int x;
     private int y;
@@ -24,6 +30,7 @@ public class Q15Diem {
     public void setY(int y) {
         if (y < -2000 || y > 2000) {
             System.out.println("Nhập y trong khoảng từ -2000 tới 2000: ");
+            return;
         } else this.y = y;
     }
 
@@ -35,8 +42,35 @@ public class Q15Diem {
         return y;
     }
 
+    public static float length(Q15Diem a, Q15Diem b) {
+        return (float) Math.sqrt((Math.pow((b.getX() - a.getX()), 2) + Math.pow(b.getY() - a.getY(), 2)));
+    }
+
+    public float comp(float a, float b) {
+        if (a > b) {
+            return a;
+        } else return b;
+    }
+
     public static void main(String[] args) {
-        Q15Diem p1 = new Q15Diem();
+        Q15Diem a = new Q15Diem(0, 1);
+        Q15Diem b = new Q15Diem(0, 2);
+        Q15Diem c = new Q15Diem(0, 3);
+        var ab = length(a, b);
+        var bc = length(b, c);
+        var ac = length(a, c);
+        ArrayList<Float> list = new ArrayList<Float>();
+        list.add(ab);
+        list.add(bc);
+        list.add(ac);
+        if ((ab + ac > bc) && (ab + bc > ac) && (ac + bc > ab)) {
+            Collections.sort(list);
+            System.out.println("Canh lon nhat dai: " + (list.get(0)));
+            System.out.println("Chu vi hinh tam giac: " + (ab + bc + ac));
+        }
+        System.out.println("Đây không phải là tam giác");
     }
 
 }
+
+
