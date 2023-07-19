9e441bf048e34fd3c218197c0f972fa182574d25
toringuyen162 <toringuyen.work@gmail.com>
2023-07-19 16:06:19 +0700

    1

--- Answers/src/main/java/Solution/Q1Solution.java
+++ Answers/src/main/java/Solution/Q1Solution.java
@@ -3,25 +3,26 @@ package Solution;
 public class Q1Solution {
     //Đề bài: tìm số nguyên tố
     public static void main(String[] args) {
-        isPrimeNumber(11.5);
+        isPrimeNumber((float) -19);
     }
 
-    public static void isPrimeNumber(int n) throws (Exception e) {
-        try {
-            int count = 1;
-            for (int i = 2; i < n; i++) {
-                if (n % i == 0) {
-                    count++;
-                }
+    public static void isPrimeNumber(float n) {
+        if (n <= 1) {
+            throw new IllegalArgumentException("n Phải là số nguyên dương lớn hơn 1");
+        }
+        int count = 0;
+        if (n == Float.valueOf(n).intValue()) {
+            count = 1;
+        }
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                count++;
             }
-            if (count == 1) {
-                System.out.println(n + " la so nguyen to");
-            } else {
-                System.out.println(n + " khong phai la so nguyen to");
-            }
-
-        } catch (Exception e) {
-            System.out.println("So nguyen to la so nguyen > 1");
+        }
+        if (count == 1) {
+            System.out.println(n + " là số nguyên tố");
+        } else {
+            System.out.println(n + " không phải là số nguyên tố");
         }
     }
 }
---
+++ Answers/src/test/java/Q1SolutionTest.java
@@ -0,0 +1,5 @@
+public class Q1SolutionTest {
+    void tc1 {
+        assert.
+    }
+}
