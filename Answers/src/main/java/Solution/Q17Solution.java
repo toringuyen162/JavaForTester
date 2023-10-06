961e602c4fb5cec998da11a529ef08a2357be4c8
Nguyễn Vân Thuỳ <toringuyen.work@gmail.com>
2023-10-06 14:12:55 +0700

    1

---
+++ Answers/src/main/java/Solution/Q17Solution.java
@@ -0,0 +1,34 @@
+package Solution;
+
+public class Q17Solution {
+
+    public static void giaiPTbac2(float a, float b, float c) {
+        if (a == 0) {
+            if (b == 0) {
+                if (c == 0) {
+                    System.out.println("Phương trình vô số nghiệm");
+                } else System.out.println("Phương trình vô nghiệm");
+            } else System.out.println("Phương trình có 1 nghiệm" + (-c / b));
+        }
+        if (a != 0) {
+            float delta = b * b - 4 * a * c;
+            float x1;
+            float x2;
+            // tính nghiệm
+            if (delta > 0) {
+                x1 = (float) ((-b + Math.sqrt(delta)) / (2 * a));
+                x2 = (float) ((-b - Math.sqrt(delta)) / (2 * a));
+                System.out.println("Phương trình có 2 nghiệm là: "
+                        + "x1 = " + x1 + " và x2 = " + x2);
+            } else if (delta == 0) {
+                x1 = (-b / (2 * a));
+                System.out.println("Phương trình có nghiệm kép: "
+                        + "x1 = x2 = " + x1);
+            } else {
+                System.out.println("Phương trình vô nghiệm!");
+            }
+        }
+    }
+}
+
+
---
+++ Answers/src/test/java/Q17SolutionTest.java
@@ -0,0 +1,11 @@
+import Solution.Q17Solution;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Q17SolutionTest {
+    @Test
+    public void testCase1{
+
+    }
+
+}
